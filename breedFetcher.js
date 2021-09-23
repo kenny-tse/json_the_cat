0446a00d7cf9b9e79873cc48d14ec5e1f096c2fb
kenny-tse <kennytse91@gmail.com>
2021-09-23 21:35:52 +0000

    added index to use breedFetcher and also test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,22 @@
 const request = require('request');
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("What breed would you want to search for? ", (answer) => {
-  console.log("You are searching for breed:", answer);
+const fetchBreedDescription = function (breedName, callback) {
   request(
-    `https://api.thecatapi.com/v1/breeds/search?q=${answer}`, (error, response, body) => {
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+
+      if (error) {
+        return callback(error, null);
+      }
 
       const data = JSON.parse(body);
 
       if (data[0]) {
-        console.log(data[0].description);
+        callback(null, data[0].description);
       } else {
-        console.log(`Breed not found!`);
-        console.log(error)
+        callback(`Breed of ${breedName} does not exist!`, null);
       }
     }
   );
-  rl.close();
-});
+};
 
+module.exports = { fetchBreedDescription };
